10dd1b3e66d73483ffd8eae857185a84a26b62e7
melch-raiol <melchraiol@hotmail.com>
2024-05-27 17:52:34 -0300

    header components finished

---
+++ src/components/header/index.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import styles from './styles.module.scss'
+import Link from 'next/link';
+
+export function Header(){
+    const [top, setTop] = useState(true);
+
+    const scrollHandle = () => {
+        window.scrollY > 10 ? setTop(false) : setTop(true);
+    }
+
+    useEffect(() =>{
+        window.addEventListener("scroll", scrollHandle)
+        return () => window.removeEventListener("scroll", scrollHandle)
+    })
+
+    return(
+        <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+           <div className={styles.container}>
+              <div className={styles.content}>
+                 <div className={styles.contentLogo}>
+                    <Link href="/">
+                        Dev Motors
+                    </Link>
+                 </div>
+
+                 <nav className={styles.nav}>
+                    <Link href="/">HOME</Link>
+                    <Link href="/#servicos">SERVIÇOS</Link>
+                    <Link href="/#contatos">CONTATOS</Link>
+                 </nav>
+              </div>
+           </div>
+        </header>
+    )
+}
